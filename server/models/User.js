31944bd2bd8253cc2f51ed96aa79e96adca1d46c
WRD <dandobilly@gmail.com>
2024-04-03 22:28:35 -0400

    partially added typedefs and resolvers and completed index

--- server/models/User.js
+++ server/models/User.js
@@ -31,7 +31,7 @@ const userSchema = new Schema({
     dogs: [
         {
             type: Schema.Types.ObjectId,
-            ref: 'dogs',
+            ref: 'dog',
         },
     ],
     vetOffice: {
