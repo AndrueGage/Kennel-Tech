09b0ad3b054af5f4746319fd24fd8740343db39f
AndrueGage <trainer.andrue@gmail.com>
2024-04-06 21:14:06 -0500

    set up proper imports for ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,60 +1,20 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
-// const multer = require('multer');
-// const storage = require('multer-gridfs-storage');
-// const url = require('mongodb://localhost:27017/kenneltech')
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs, resolvers } from './schemas/index.js'
 
-const { typeDefs, resolvers } = require('./schemas');
-const db =  require('./config/connection');
+import { connectToDatabase } from './config/connection.js';
 
-// const storage = new GridFsStorage({ url }); // Create a storage object with a given configuration
-// const upload = multer({ storage }); // Set multer storage engine to the newly created object
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-// Upload your files as usual
-// app.post('/profile', upload.single('avatar'), (req, res, next) => { 
-//     /*....*/ 
-// });
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async () => {
+    const db = await connectToDatabase();
+    return { db };
+  },
+});
 
-// app.post('/photos/upload', upload.array('photos', 12), (req, res, next) => {
-//     /*....*/ 
-// });
-
-// app.post('/cool-profile', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]), (req, res, next) => {
-//     /*....*/ 
-// });
-
-
-const PORT = process.env.PORT || 5173;
-const app = express();
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-// });
-
-// const startApolloServer = async () => {
-//     await server.start();
-
-//     app.use(express.urlencoded({ extended: true }));
-//     app.use(express.json());
-
-//     app.use('/graphql', expressMiddleware(server));
-
-//     if (process.env.NODE_ENV === 'production') {
-//         app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//         app.get('*', (req, res) => {
-//             res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//         });
-//     }
-
-//     db.once('open', () => {
-//         app.listen(PORT, () => {
-//             console.log(`API server running on port ${PORT}!`);
-//             console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-//         });
-//     });
-// };
-
-// startApolloServer();
+console.log(`🚀  Server ready at: ${url}`);
