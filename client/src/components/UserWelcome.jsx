64329db94a48c25bf1b9f0932f1409ec9f3c4be1
AndrueGage <trainer.andrue@gmail.com>
2024-04-09 00:16:41 -0500

    added welcome user component

---
+++ client/src/components/UserWelcome.jsx
@@ -0,0 +1,18 @@
+import auth from '../utils/auth'
+import { useQuery } from '@apollo/client'
+import { QUERY_USER } from '../utils/queries'
+
+export default function UserWelcome() {
+    const user = auth.getUser()
+
+    const { data, error } = useQuery(QUERY_USER, {
+        variables: {
+            id: user.data._id
+        }
+    })
+
+    return (
+        <p className="bg-neutral-300 text-xl font-semibold py-2 px-3 rounded-lg">Welcome {data && data.getUserById.firstName + '!'}</p>
+    )
+
+}
