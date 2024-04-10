02795f946aa33cf3f6b3f8c3c3c776b679adf2d0
AndrueGage <trainer.andrue@gmail.com>
2024-04-10 18:42:18 -0500

    mongodb uri update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb://localhost:27017/kenneltech';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/kenneltech';
+
+
 
 async function connectToDatabase() {
   try {
