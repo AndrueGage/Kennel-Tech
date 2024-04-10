107fbdf8c10fc4e014c979b3990b321a1c93ea74
AndrueGage <trainer.andrue@gmail.com>
2024-04-09 20:52:51 -0500

    added contributor page data and styling

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -2,6 +2,7 @@ import ClientNav from '../components/ClientNav';
 import { useAuth } from '../utils/AuthContext';
 import { Link } from 'react-router-dom';
 import Container from '../components/Container';
+import GitHub from '../assets/GitHub-logo.png'
 
 export default function ContactPage() {
     const { loggedIn } = useAuth();
@@ -10,6 +11,14 @@ export default function ContactPage() {
         // Handle case when user is not logged in
         return <p>Please <Link to="/login"> log in</Link></p>;
     }
+
+    const contributors = [
+        { name: 'Andrue Desmarais', github: 'https://github.com/AndrueGage' },
+        { name: 'Michael Gerlach', github: 'https://github.com/Gerlach0130' },
+        { name: 'Nicholas Holder', github: 'https://github.com/nickholder6425' },
+        { name: 'William Dando', github: 'https://github.com/BrdwrdI' },
+    ]
+    const repoLink = [{name: 'Kennel Tech Repository', github: 'https://github.com/AndrueGage/Kennel-Tech'}]
     return (
         <div className='max-w-[1400px] mx-auto flex flex-col gap-8 my-5'>
             <ClientNav />
@@ -18,6 +27,20 @@ export default function ContactPage() {
             <div className="border-2 border-neutral-800 rounded-xl p-8 flex flex-col gap-3 justify-between">
                 <h2 className='font font-semibold text-lg '>Contributor Links</h2>
                 <hr className='w-full h-1 inline-block' />
+                {contributors.map(contributor => (
+                    <div key={contributor.name} className='flex items-center' >
+                        <img className='w-24 h-24 mr-4' src={GitHub} alt='GitHub logo' />
+                        <a href={contributor.github} target='_blank' className='text-lg font-semibold'>{contributor.name}</a>
+                    </div>
+                ))}
+                <h2 className='font font-semibold text-lg '>This Projects Repository</h2>
+                <hr className='w-full h-1 inline-block' />
+                {repoLink.map(repoLink =>(
+                <div key={repoLink.name} className='flex items-center' >
+                        <img className='w-24 h-24 mr-4' src={GitHub} alt='GitHub logo' />
+                        <a href={repoLink.github} target='_blank' className='text-lg font-semibold'>{repoLink.name}</a>
+                    </div>
+                    ))}
             </div>
         </div>
     )
